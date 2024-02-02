40629fa68f0ac1bc95ab9d6231edb0986abf9a91
Venkatesh Thalluri <venkateshthalluri123@gmail.com>
2024-02-02 20:33:57 +0530

    initial commit from springboot app template

---
+++ src/main/java/com/deliveroo/expressionengine/ExpressionEngineApplication.java
@@ -0,0 +1,13 @@
+package com.deliveroo.expressionengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressionEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressionEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deliveroo/expressionengine/ExpressionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveroo.expressionengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
